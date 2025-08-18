d014b461af3bcd6cf26e53ad9b2a4346608f22dd
Senthil Nathan.R <Senthil Nathan.R@LAPTOP-GGNJHGC9>
2025-08-18 13:14:37 +0530

    day1

---
+++ C2TC_CoreJava/src/Day1/IdentifersDemo.java
@@ -0,0 +1,15 @@
+package Day1;
+
+public class IdentifersDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int $number = 07; //starts with $
+		System.out.println("value of the number variable is : "+ $number);
+	
+		String studentName="DEEPAK KUMAR S"; //use camelcase
+		System.out.println("value is : "+ studentName);
+
+	}
+
+}
